b965364170a75f3d060b9977811606fc6ce15c62
Zayn Ahmad <143194096+Zayn-Alabeden-Ahamd@users.noreply.github.com>
2024-12-05 15:11:41 +0300

    Add files via upload
    LAST EDITS ON DJANGO DATABASE

--- Django/softwareEng2Project/login/loginsys/views.py
+++ Django/softwareEng2Project/login/loginsys/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Log
+from django.http import JsonResponse
+import json
 # Create your views here.
 def index(request):
     users = Log.objects.all()
     context = {'users':users}
     return render(request,'logg/index.html',context)
+def json(requset):
+    data =  list(Log.objects.values())
+    return JsonResponse(data,safe=False)
